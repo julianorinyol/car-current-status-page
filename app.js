a5811ceeb0456f01680f7be4de46db65e717af89
Julian Orinyol <julianorinyol@gmail.comm>
2019-12-09 15:25:48 +0100

    chore: remove socket.io chat boilerplate

--- app.js
+++ app.js
@@ -5,21 +5,6 @@ app.use(bodyParser.json()) // for parsing application/json
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const port = 3333
-// io.on('connection', function(socket){
-// 	console.log('a user connected');
-// 	socket.broadcast.emit('hi');
-// 	socket.on('chat message', function(msg){
-// 		console.log('message: ' + msg);
-
-// 	});
-// });
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-  	console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
-});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
