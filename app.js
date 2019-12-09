fdd32287bd2fc802bb4440bef4fd779cf195411b
Julian Orinyol <julianorinyol@gmail.comm>
2019-12-09 11:50:59 +0100

    feature: basic socket stuff working, with jQuery chat example

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
+
 var app = require('express')();
+const bodyParser = require('body-parser')
+app.use(bodyParser.json()) // for parsing application/json
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+const port = 3333
+// io.on('connection', function(socket){
+// 	console.log('a user connected');
+// 	socket.broadcast.emit('hi');
+// 	socket.on('chat message', function(msg){
+// 		console.log('message: ' + msg);
+
+// 	});
+// });
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+  	console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+
+app.post('/status', function(req, res){
+  res.send('ok');
+  console.log(req.body)
+  io.emit('status update', req.body);
 });
+
+http.listen(port, function(){
+  console.log(`listening on *:${port}`);
+});
+
