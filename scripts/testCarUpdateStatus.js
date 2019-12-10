662b2d5942c06312cb8a1b397cacdd1c8d58d4f7
Julian Orinyol <julianorinyol@gmail.comm>
2019-12-10 12:20:01 +0100

    feature: update testCarUpdateStatus to send joystick status and car status. also add command line option to either send single or continuously.

--- scripts/testCarUpdateStatus.js
+++ scripts/testCarUpdateStatus.js
@@ -3,81 +3,72 @@ const axios = require('axios');
 const port = 3333
 const host = `http://localhost:${port}`
 
-const endpoint = `${host}/carstatus`
-
-
+const statusEndpoint = `${host}/status`
+const carStatusEndpoint = `${host}/carstatus`
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-const postStatusUpdate = () => {
-	const postBody = {
+const range = (total, start) => {
+ return [...Array(total + start).keys()].slice(start)
+}
+
+const postJoystickStatusUpdate = () => {
+	const body = {	
+		"joystick.buttons.a": getRandomInt(2),
+		"joystick.buttons.b": getRandomInt(2),
+		"joystick.buttons.c": getRandomInt(2),
+		"joystick.buttons.d": getRandomInt(2),
+	}
+
+	return post(statusEndpoint, body)
+}
+
+
+const post = (endpoint, body) => {
+	return axios.post(endpoint, body)
+	.then(function (response) {
+	  console.log(`posting data to ${endpoint} was successful.`)
+	})
+	.catch(function (error) {
+	  console.error(`Error posting to ${endpoint}`, body, error, );
+	  throw error 
+	});	
+}
+
+const postCarStatusUpdate = () => {
+	// "test item key 1": getRandomInt(1000),
+
+	const body = {	
 		cruiseState: {
 		  "enabled": false,
 		  "available": false,
 		  "speedOffset": 0.0,
 		  "speed": 0.0,
 		  "standstill": false
-		},
-		"test item key 1": getRandomInt(1000),
-		"test item key 2": getRandomInt(1000),
-		"test item key 3": getRandomInt(2),
-		"test item key 4": getRandomInt(2),
-		"test item key 5": 0,
-		"test item key 6": 0,
-		"test item key 7": getRandomInt(1000),
-		"test item key 8": getRandomInt(1000),
-		"test item key 9": getRandomInt(2),
-		"test item key 10": getRandomInt(2),
-		"test item key 11": 0,
-		"test item key 12": 0,
-		"test item key 13": getRandomInt(1000),
-		"test item key 14": getRandomInt(1000),
-		"test item key 15": getRandomInt(2),
-		"test item key 16": getRandomInt(2),
-		"test item key 17": 0,
-		"test item key 18": 0,
-		"test item key 19": getRandomInt(1000),
-		"test item key 20": getRandomInt(1000),
-		"test item key 21": getRandomInt(2),
-		"test item key 22": getRandomInt(2),
-		"test item key 23": 0,
-		"test item key 24": 0,
-		"test item key 25": getRandomInt(1000),
-		"test item key 26": getRandomInt(1000),
-		"test item key 27": getRandomInt(2),
-		"test item key 28": getRandomInt(2),
-		"test item key 29": 0,
-		"test item key 30": 0,
-		"test item key 31": getRandomInt(1000),
-		"test item key 32": getRandomInt(1000),
-		"test item key 33": getRandomInt(2),
-		"test item key 34": getRandomInt(2),
-		"test item key 35": 0,
-		"test item key 36": 0,
-		"test item key 37": getRandomInt(1000),
-		"test item key 38": getRandomInt(1000),
-		"test item key 39": getRandomInt(2),
-		"test item key 40": getRandomInt(2),
-		"test item key 41": 0,
-		"test item key 42": 0,
-		"test item key 43": getRandomInt(1000),
-		"test item key 44": getRandomInt(1000),
-		"test item key 45": getRandomInt(2),
-		"test item key 46": getRandomInt(2),
-		"test item key 47": 0,
-		"test item key 48": 0,
+		}
+	}
 
-	}	
-	return axios.post(endpoint, postBody)
-	.then(function (response) {
-	  console.log(response.data);
+	// just generating some nice test data... 
+	range(20, 1).forEach(num => {
+		body[`test item key ${num}`] = 	getRandomInt(1000)
 	})
-	.catch(function (error) {
-	  console.log(error);
-	});	
+
+	// generating boolean basically either 1 or 0, makes it easy to test the falsey circle styles
+	range(20, 11).forEach(num => {
+		body[`test item key ${num}`] = 	getRandomInt(2)
+	})
+	
+
+	return post(carStatusEndpoint, body)
 }
 
-// setInterval(postStatusUpdate, 500);
-postStatusUpdate()
+
+if(process.argv.length === 3) {
+	postCarStatusUpdate()
+	postJoystickStatusUpdate()
+} else {
+	setInterval(postJoystickStatusUpdate, 300);
+	setInterval(postCarStatusUpdate, 500);		
+}
